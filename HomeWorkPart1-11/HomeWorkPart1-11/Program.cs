f7ac0f61c60d1bd5ba77790f8ac355a0529d4849
AlexandRLV <cahbka13@gmail.com>
2016-09-23 19:28:52 +0300

    HomeWorkPart1-11

---
+++ HomeWorkPart1-11/HomeWorkPart1-11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkPart1_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = Convert.ToDouble(Console.ReadLine()),
+            y1 = Convert.ToDouble(Console.ReadLine()),
+            x2 = Convert.ToDouble(Console.ReadLine()),
+            y2 = Convert.ToDouble(Console.ReadLine()),
+            x3 = Convert.ToDouble(Console.ReadLine()),
+            y3 = Convert.ToDouble(Console.ReadLine()), s;
+
+
+            s = 0.5 * Math.Abs((x1 - x3) * (y2 - y3) - (x2 - x3) * (y1 - y3));
+            Console.WriteLine(s);
+        }
+    }
+}
