0f52cecb442bbf244518ba00f27012ded41e8b80
AlexandRLV <cahbka13@gmail.com>
2016-09-23 19:34:18 +0300

    HomeWorkPart2-6

---
+++ HomeWorkPart2-6/HomeWorkPart2-6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkPart2_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, s = 0;
+            for (i = 1; i < 1000; i++)
+            {
+                if ((i % 3 == 0) || (i % 5 == 0))
+                {
+                    s += i;
+                }
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
