dad3e0bca4636cdd138d778b4ec5e18e325e4537
AlexandRLV <cahbka13@gmail.com>
2016-09-23 20:07:00 +0300

    HomeWork

---
+++ HomeWorkPart4-14/HomeWorkPart4-14/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkPart4_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, n;
+            n = Convert.ToInt32(Console.ReadLine());
+            a = 1;
+            for (int i=1;i<=n;i++)
+            {
+                a *= (i % 10);
+                a %= 10;
+            }
+            Console.WriteLine(a);
+        }
+    }
+}
