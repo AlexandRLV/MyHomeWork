0ed52759f25e2255e74a9b08021a5a8d41c2cccf
Александр <@>
2016-10-11 20:58:41 +0300

    HW

---
+++ HomeWork2Part1-12/HomeWork2Part1-12/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2Part1_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, c, e, s, k, p, t;
+            x = Convert.ToDouble(Console.ReadLine());
+            e = Convert.ToDouble(Console.ReadLine());
+
+            c = Math.Cos(x);
+            c *= c;
+            s = 1;
+            k = 1;
+            p = 2;
+            t = 1;
+
+
+            while (Math.Abs(s-c)>e)
+            {
+                p *= (-1) * 4 * x * x;
+                t *= (2 * k) * (2 * k - 1);
+                s += p / t;
+                k++;
+            }
+
+            Console.WriteLine("Steps: {0}", k);
+        }
+    }
+}
---
+++ HomeWork2Part1-6/HomeWork2Part1-6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2Part1_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, p, k, e, s,t,a;
+            x = Convert.ToDouble(Console.ReadLine());
+            e = Convert.ToDouble(Console.ReadLine());
+
+            k = 1;
+            s = x;
+            p = x;
+            a = 1;
+            t = Math.Tan(x);
+
+            while (Math.Abs(s-t)>e)
+            {
+                p *= (-1)*x * x;
+                a += 2;
+                s += p / a;
+                k++;
+            }
+
+            Console.WriteLine("Steps: {0}", k);
+        }
+    }
+}
