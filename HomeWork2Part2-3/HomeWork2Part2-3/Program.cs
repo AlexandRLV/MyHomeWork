b51e08261d4f823568984cae99878497ff5c3148
Александр <@>
2016-10-11 21:24:39 +0300

    HW

---
+++ HomeWork2Part2-3/HomeWork2Part2-3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2Part2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double s, pi, e, a, b,k;
+            pi = Math.PI;
+            e = Convert.ToDouble(Console.ReadLine());
+
+            s = 1;
+            a = 1;
+            b = 1;
+            k = 1;
+
+            while (Math.Abs(s - pi) > e)
+            {
+                a *= -1;
+                b *= 3;
+                s += a / (b * (2 * k + 1));
+                k++;
+            }
+
+            Console.WriteLine("Steps: {0}, Result: {1}", k, s);
+        }
+    }
+}
