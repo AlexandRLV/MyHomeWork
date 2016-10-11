c32c1f677d0d8b0356e8c88ac2521813203a08b2
Александр <@>
2016-10-11 21:04:11 +0300

    HW

--- HomeWork2Part1-19/HomeWork2Part1-19/Program.cs
+++ HomeWork2Part1-19/HomeWork2Part1-19/Program.cs
@@ -10,7 +10,26 @@ namespace HomeWork2Part1_19
     {
         static void Main(string[] args)
         {
+            double pi, k, x, e, s, p;
+            pi = Math.PI;
+            pi *= pi;
+            pi /= 18;
+            e = Convert.ToDouble(Console.ReadLine());
 
+            s = 0;
+            k = 2;
+            p = 1;
+            x = 1;
+
+            while (Math.Abs(s - pi) > e)
+            {
+                p *= (k - 1) * (k - 1);
+                x *= (2 * k) * (2 * k - 1);
+                s += p / x;
+                k++;
+            }
+
+            Console.WriteLine("Steps: {0}", k);
         }
     }
 }
